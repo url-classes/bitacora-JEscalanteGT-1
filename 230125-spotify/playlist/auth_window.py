1285b6a174c6470a653c51c134a170f60aaa6cc9
Jorge Escalante <hi@jescalante.me>
2023-02-17 08:33:47 -0600

    Agrega PyQt6

---
+++ 230125-spotify/playlist/auth_window.py
@@ -0,0 +1,22 @@
+from PyQt6.QtWidgets import (
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget
+)
+
+
+class AuthWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        login_button = QPushButton('Ingresar')
+        refresh_button = QPushButton('Volver a intentar')
+
+        layout = QVBoxLayout()
+        layout.addWidget(login_button)
+        layout.addWidget(refresh_button)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
--- 230125-spotify/playlist/main.py
+++ 230125-spotify/playlist/main.py
@@ -1,16 +1,87 @@
 import os
+import sys
 import base64
 import requests
-
+from typing import Optional
+from PyQt6.QtWidgets import QApplication
+from auth_window import AuthWindow
+from playlist_window import PlaylistWindow
 
 client_id = ''
 client_secret = ''
 
-while True:
+app = QApplication(sys.argv)
+auth_window = AuthWindow()
+playlist_window = PlaylistWindow()
+
+
+def generate_token(code: str) -> str | None:
+    credentials = base64.b64encode(
+        client_id.encode()
+        + b':'
+        + client_secret.encode()
+    ).decode('utf-8')
+
+    token_headers = {
+        'Authorization': 'Basic ' + credentials,
+        'content-type': 'application/x-www-form-urlencoded'
+    }
+
+    token_data = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': 'http://localhost:7777/callback'
+    }
+
+    token_response = requests.post(
+        'https://accounts.spotify.com/api/token',
+        data=token_data,
+        headers=token_headers
+    ).json()
+
+    if os.path.isfile('../code.txt'):
+        os.remove('../code.txt')
+
+    if 'error' in token_response:
+        return None
+    else:
+        token = token_response['access_token']
+        file = open('../token.txt', 'w')
+        file.write(token)
+        file.close()
+
+        return token
+
+
+def get_code() -> str | None:
+    if os.path.isfile('../code.txt'):
+        file = open('../code.txt')
+        code = file.read()
+        file.close()
+        return code
+
+    return None
+
+
+def get_token() -> str | None:
     if os.path.isfile('../token.txt'):
-        f = open('../token.txt', 'r')
-        token = f.read()
-        f.close()
+        file = open('../token.txt')
+        token = file.read()
+        file.close()
+        return token
+    elif os.path.isfile('../code.txt'):
+        code = get_code()
+        if code is None:
+            return None
+        else:
+            return get_token()
+
+
+def check_permissions() -> str | None:
+    user_id: Optional[str] = None
+    token = get_token()
+
+    if token is not None:
         user_headers = {
             'Authorization': 'Bearer ' + token,
             'Content-type': 'application/json'
@@ -20,113 +91,22 @@ while True:
             headers=user_headers
         ).json()
 
-        if 'error' in user_profile_response:
-            print(user_profile_response['error']['message'])
-            os.remove('../token.txt')
-            print('El archivo ../token.txt ha sido eliminado')
-        else:
+        if not ('error' in user_profile_response):
             user_id = user_profile_response['id']
-            user_uri = user_profile_response['uri']
-            if os.path.isfile('../playlist.txt'):
-                f = open('../playlist.txt', 'r')
-                playlist_id = f.read()
-                f.close()
-                endpoint = f'https://api.spotify.com/v1/playlists/{playlist_id}'
-                get_playlist_response = requests.get(
-                    endpoint,
-                    headers=user_headers
-                ).json()
-                playlist_id = get_playlist_response['id']
-                playlist_uri = get_playlist_response['uri']
-                print('Playlist ID:', playlist_id)
-                print('Playlist URI:', playlist_uri)
+        elif 'error' in user_profile_response and os.path.isfile('../token.txt'):
+            os.remove('../token.txt')
 
-                endpoint = 'https://api.spotify.com/v1/search'
-                search_response = requests.get(
-                    endpoint,
-                    headers=user_headers,
-                    params={
-                        'q': 'Bad bunny',
-                        'type': 'album,track',
-                        'limit': 5,
-                        'include_external': 'audio'
-                    }
-                ).json()
-                print('Resultados de la búsqueda:')
-                tracks = search_response['tracks']['items']
-                # duration_ms
-                # ID
-                # name
-                canciones = []
-                for track in tracks:
-                    print(track['id'])
-                    print(track['name'])
-            else:
-                endpoint = f'https://api.spotify.com/v1/users/{user_id}/playlists'
-                body = {
-                    "name": "Mi nueva playlist",
-                    "description": "Esta playlist es del curso de Estructura de Datos I"
-                }
+    return user_id
 
-                create_playlist_response = requests.post(
-                    endpoint,
-                    json=body,
-                    headers=user_headers
-                ).json()
 
-                playlist_id = create_playlist_response['id']
-                f = open('../playlist.txt', 'w')
-                f.write(playlist_id)
-                print('Se ha creado una playlist:', playlist_id)
-
-    elif os.path.isfile('../code.txt'):
-        f = open('../code.txt', 'r')
-        code = f.read()
-        f.close()
-        credentials = base64.b64encode(
-            client_id.encode()
-            + b':'
-            + client_secret.encode()
-        ).decode('utf-8')
-
-        token_headers = {
-            'Authorization': 'Basic ' + credentials,
-            'content-type': 'application/x-www-form-urlencoded'
-        }
-
-        token_data = {
-            'grant_type': 'authorization_code',
-            'code': code,
-            'redirect_uri': 'http://localhost:7777/callback'
-        }
-
-        r = requests.post(
-            'https://accounts.spotify.com/api/token',
-            data=token_data,
-            headers=token_headers
-        ).json()
-
-        if 'error' in r:
-            print('Su código de acceso a expirado.')
-            print('Vuelva a acceder a http://localhost:7777/authorize')
-        else:
-            token = r['access_token']
-            f = open('../token.txt', 'w')
-            f.write(token)
-            f.close()
-            print('El token de acceso ha sido generado.')
-
-        try:
-            os.remove('../code.txt')
-            print('El archivo ../code.txt ha sido eliminado')
-        except Exception as error:
-            print('No pude eliminar el archivo ../code.txt')
-            print('Error:', error)
-
+def main():
+    user_id = check_permissions()
+    if user_id is None:
+        auth_window.show()
     else:
-        print('No hay credenciales de acceso.')
-        print('Acceda a: http://localhost:7777/authorize')
+        playlist_window.show()
 
-    option = input('¿Volver a intentar? (s/n): ')
-    if option == 'n':
-        break
+    app.exec()
+
+
+main()
