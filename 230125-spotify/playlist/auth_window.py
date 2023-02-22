e207d26e7a820ed38ccfecc4a30db5ba3e76a2d4
Jorge Escalante <hi@jescalante.me>
2023-02-21 22:29:32 -0600

    Agrega visualización básica de playlist

--- 230125-spotify/playlist/auth_window.py
+++ 230125-spotify/playlist/auth_window.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 from PyQt6.QtWidgets import (
     QMainWindow,
     QPushButton,
@@ -5,12 +7,21 @@ from PyQt6.QtWidgets import (
     QWidget
 )
 
+from communicate import Communicate
+
 
 class AuthWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, communicate: Communicate):
         super().__init__()
+
+        self.setWindowTitle('Iniciar sesión')
+        self.communicate = communicate
+
         login_button = QPushButton('Ingresar')
+        login_button.clicked.connect(self.open_browser)
+
         refresh_button = QPushButton('Volver a intentar')
+        refresh_button.clicked.connect(self.try_again)
 
         layout = QVBoxLayout()
         layout.addWidget(login_button)
@@ -20,3 +31,9 @@ class AuthWindow(QMainWindow):
         widget.setLayout(layout)
 
         self.setCentralWidget(widget)
+
+    def open_browser(self):
+        webbrowser.open('http://localhost:7777/authorize')
+
+    def try_again(self):
+        self.communicate.verify_code.emit()
---
+++ 230125-spotify/playlist/communicate.py
@@ -0,0 +1,5 @@
+from PyQt6.QtCore import QObject, pyqtSignal
+
+
+class Communicate(QObject):
+    verify_code = pyqtSignal()
